3ee10bc1dfad96fe453aed2629291f850d4caae5
edenede2 <131803027+edenede2@users.noreply.github.com>
2025-08-30 19:08:11 +0300

    Enhance enrichment analysis functions with keytype guessing and improved ENTREZ mapping; update output file names for ROSMAP dataset.

--- old_scripts/enrichment.R
+++ old_scripts/enrichment.R
@@ -4,8 +4,53 @@ library(R.utils)
 R.utils::setOption("clusterProfiler.download.method",'auto')
 library(clusterProfiler)
 library(purrr)
+library(AnnotationDbi)
 
 #####------ GO enrichment of modules-------#######
+
+normalize_gene_ids <- function(x) {
+  x <- as.character(x)
+  x <- trimws(x)
+  # drop Ensembl version suffix
+  sub("\\.\\d+$", "", x)
+}
+
+guess_keytype_and_keys <- function(ids) {
+  ids <- normalize_gene_ids(ids)
+  is_ensg <- grepl("^ENSG\\d+", ids)
+  frac_ensg <- if (all(is.na(is_ensg))) 0 else mean(is_ensg, na.rm = TRUE)
+  if (!is.nan(frac_ensg) && frac_ensg > 0.5) {
+    list(keytype = "ENSEMBL", keys = ids)
+  } else {
+    list(keytype = "SYMBOL", keys = ids)
+  }
+}
+
+# Helper: map to ENTREZ with validation
+
+map_to_entrez <- function(keys_vec, keytype) {
+  k <- unique(keys_vec[!is.na(keys_vec) & nzchar(keys_vec)])
+  if (!length(k)) stop("Empty key vector after filtering NAs/empties.")
+  # primary attempt
+  valid <- tryCatch(AnnotationDbi::keys(org.Hs.eg.db, keytype = keytype),
+                    error = function(e) character())
+  use <- intersect(k, valid)
+  if (length(use) > 0) {
+    return(AnnotationDbi::select(org.Hs.eg.db, keys = use, columns = "ENTREZID", keytype = keytype))
+  }
+  # fallback: try the other common keytype
+  alt <- if (identical(keytype, "ENSEMBL")) "SYMBOL" else "ENSEMBL"
+  valid_alt <- tryCatch(AnnotationDbi::keys(org.Hs.eg.db, keytype = alt),
+                        error = function(e) character())
+  use_alt <- intersect(k, valid_alt)
+  if (length(use_alt) > 0) {
+    message(sprintf("Primary keytype '%s' failed; falling back to '%s'.", keytype, alt))
+    return(AnnotationDbi::select(org.Hs.eg.db, keys = use_alt, columns = "ENTREZID", keytype = alt))
+  }
+  stop(sprintf("No valid keys for keytype='%s' or fallback. First few ids: %s",
+               keytype, paste(utils::head(k, 5), collapse = ", ")))
+}
+`%||%` <- function(a, b) if (!is.null(a)) a else b
 
 # Remove GO terms that are prevalent across modules
 removePrevalentTerms = function(enrich_tab, max_preval) {
@@ -28,43 +73,99 @@ filterCommonGOTerms = function(go, max_preval=50) {
 enrichmentGO = function(modules) {
   require(org.Hs.eg.db)
   require(WGCNA)
-  # Map ensembl IDs
-  ensembl = modules$Gene.Symbol
+  stopifnot(all(c("Gene.Symbol", "Cluster.ID") %in% colnames(modules)))
+
+  ids <- normalize_gene_ids(modules$Gene.Symbol)
+  id_info <- guess_keytype_and_keys(ids)
+  keytype <- id_info$keytype
+  keys_vec <- id_info$keys
+
+  
+  # # Map ensembl IDs
+  # ensembl = modules$Gene.Symbol
   
   # Load map of Ensembl -> ENTREX IDs
-  entrez_map = AnnotationDbi::select(org.Hs.eg.db, ensembl, "ENTREZID", "ENSEMBL")
+  # entrez_map = AnnotationDbi::select(org.Hs.eg.db, ensembl, "ENTREZID", "ENSEMBL")
+  entrez_map = map_to_entrez(keys_vec, keytype = keytype)
+  map_key_col <- if ("ENSEMBL" %in% colnames(entrez_map)) "ENSEMBL" else "SYMBOL"
+
+  # entrez = entrez_map$ENTREZID[match(ensembl, entrez_map$ENSEMBL)]
+  entrez = entrez_map$ENTREZID[match(keys_vec, entrez_map[[map_key_col]])]
+
+  keep <- !is.na(entrez) & nzchar(entrez)
+  if (!any(keep)) stop("No genes mapped to ENTREZ IDs.")
+
+  GOenrichmentAnalysis(
+    modules$Cluster.ID[keep],
+    entrez[keep],
+    organism="human",
+    removeDuplicates=FALSE,
+    nBestP=10,
+    backgroundType = "givenInGO"
+  )
+
+  # # go_enrich = GOenrichmentAnalysis(between$Cluster.ID,
+  # #                                   entrez,
+  # #                                   organism="human",
+  # #                                   #ontologies = "BP",
+  # #                                   removeDuplicates=FALSE,
+  # #                                   nBestP=10,
+  # #                                   backgroundType = "givenInGO"
+  # #                                   #pCut=0.05  
+  # #  )
   
-  entrez = entrez_map$ENTREZID[match(ensembl, entrez_map$ENSEMBL)]
-  
-  go_enrich = GOenrichmentAnalysis(between$Cluster.ID,
-                                    entrez,
-                                    organism="human",
-                                    #ontologies = "BP",
-                                    removeDuplicates=FALSE,
-                                    nBestP=10,
-                                    backgroundType = "givenInGO"
-                                    #pCut=0.05  
-   )
-  
-  return(go_enrich)
+  # return(go_enrich)
   
 }
   
 #####------ Kegg enrichment of modules-------#######
-  enrichmentKegg = function(modules) {
- 
-  between1 = merge(modules, entrez_map, by.x=c("Gene.Symbol"), by.y=c("ENSEMBL"))
-  kegg_enrich = compareCluster(geneClusters = ENTREZID~Cluster.ID, data=between1, fun = "enrichKEGG" , organism = "hsa"  , keyType = "kegg", pvalueCutoff = 1, qvalueCutoff = 1)
-  kegg_enrich = setReadable(kegg_enrich, OrgDb = org.Hs.eg.db, keyType="ENTREZID")
+enrichmentKegg = function(modules) {
+  require(org.Hs.eg.db)
+  stopifnot(all(c("Gene.Symbol", "Cluster.ID") %in% colnames(modules)))
+  modules$Gene.ID.norm <- normalize_gene_ids(modules$Gene.Symbol)
+
+  id_info <- guess_keytype_and_keys(modules$Gene.ID.norm)
+  keytype <- id_info$keytype
+  keys_vec <- id_info$keys
+
+  entrez_map <- map_to_entrez(keys_vec, keytype = keytype)
+  map_key_col <- if ("ENSEMBL" %in% colnames(entrez_map)) "ENSEMBL" else "SYMBOL"
+  # Drop NAs, deduplicate key->ENTREZ (keep first if 1:many)
+  ent_clean <- entrez_map[!is.na(entrez_map$ENTREZID) & nzchar(entrez_map$ENTREZID), c(map_key_col, "ENTREZID")]
+  ent_clean <- ent_clean[!duplicated(ent_clean), , drop = FALSE]
+
+  m <- merge(modules, ent_clean, by.x = "Gene.ID.norm", by.y = map_key_col)
+  m <- m[!is.na(m$ENTREZID) & nzchar(m$ENTREZID), , drop = FALSE]
+  if (!nrow(m)) {
+    stop(sprintf(
+      "No genes mapped to ENTREZ for KEGG. Example inputs: %s",
+      paste(utils::head(unique(modules$Gene.ID.norm), 5), collapse = ", ")
+    ))
+  }
+
+  kegg_enrich <- compareCluster(
+    geneClusters = ENTREZID ~ Cluster.ID,
+    data = m,
+    fun = "enrichKEGG",
+    organism = "hsa",
+    keyType = "ncbi-geneid",
+    pvalueCutoff = 1,
+    qvalueCutoff = 1
+  )
+  setReadable(kegg_enrich, OrgDb = org.Hs.eg.db, keyType="ENTREZID")
+
+  # between1 = merge(modules, entrez_map, by.x=c("Gene.Symbol"), by.y=c("ENSEMBL"))
+  # kegg_enrich = compareCluster(geneClusters = ENTREZID~Cluster.ID, data=between1, fun = "enrichKEGG" , organism = "hsa"  , keyType = "kegg", pvalueCutoff = 1, qvalueCutoff = 1)
+  # kegg_enrich = setReadable(kegg_enrich, OrgDb = org.Hs.eg.db, keyType="ENTREZID")
   
   
-  return(kegg_enrich)
+  # return(kegg_enrich)
 }
 
 
 # ---------------------------------------------
-Clusters_table = "/Users/edeneldar/CoExpression_ReProduction/xwgcna_young_original_run9_Clusters_table.txt"
-Clusters_details <- read.delim("/Users/edeneldar/CoExpression_ReProduction/xwgcna_young_original_run9_Clusters_details.txt",sep="\t", header=T)
+Clusters_table = "/Users/edeneldar/Library/CloudStorage/GoogleDrive-edenede2@gmail.com/Shared drives/is-SomekhLab/inter_tissue_coexpression/proccessed_data/ROSMAP_full_outputs/xwgcna_rosmap_autobeta_run4_Cluster_table.tsv"
+Clusters_details <- read.delim("/Users/edeneldar/Library/CloudStorage/GoogleDrive-edenede2@gmail.com/Shared drives/is-SomekhLab/inter_tissue_coexpression/proccessed_data/ROSMAP_full_outputs/xwgcna_rosmap_autobeta_run4_Cluster_details.tsv",sep="\t", header=T)
 Clusters_table <- read.delim(Clusters_table,sep="\t", header=T)
 
 go_enrich = enrichmentGO(Clusters_table)
@@ -106,32 +207,32 @@ go_tab = data.frame(
 
 
 write.table(kegg_enrich@compareClusterResult,
-            "kegg_young.csv",
+            "kegg_rosmap_full.csv",
             sep=",",
             quote=FALSE,
             row.names=FALSE)
 
 write.table(go_enrich_filter$bestPTerms$BP$enrichment,
-            "go_bp_tab_young.csv",
+            "go_bp_tab_rosmap_full.csv",
             sep=",",
             quote=FALSE,
             row.names=FALSE)
 
 write.table(go_enrich_filter$bestPTerms$CC$enrichment,
-            "go_cc_tab_young.csv",
+            "go_cc_tab_rosmap_full.csv",
             sep=",",
             quote=FALSE,
             row.names=FALSE)
 
 write.table(go_enrich_filter$bestPTerms$MF$enrichment,
-            "go_mf_tab_young.csv",
+            "go_mf_tab_rosmap_full.csv",
             sep=",",
             quote=FALSE,
             row.names=FALSE)
 
 
 write.table(go_enrich_filter$enrichmentP,
-            "go_pmat_young.tsv",
+            "go_pmat_rosmap_full.tsv",
             sep="\t")
 
 cluster_summary <- data.frame(go_enrich)
@@ -142,91 +243,12 @@ fit
 
 
 
-library(WGCNA)
-library(org.Hs.eg.db)
-library(R.utils)
-R.utils::setOption("clusterProfiler.download.method", "auto")
-library(clusterProfiler)
-library(purrr)
-
-# Remove GO terms that are prevalent across modules
-removePrevalentTerms <- function(enrich_tab, max_preval) {
-  mod_counts <- table(enrich_tab$termID)
-  include_idx <- mod_counts < max_preval
-  message("Excluding GO terms: ", sum(!include_idx))
-  include_terms <- names(which(include_idx))
-  enrich_tab[enrich_tab$termID %in% include_terms, ]
-}
-
-# Filter out GO terms from WGCNA GO enrichment analysis
-filterCommonGOTerms <- function(go, max_preval = 50) {
-  go$bestPTerms$BP$enrichment <-
-    removePrevalentTerms(go$bestPTerms$BP$enrichment, max_preval)
-  go$bestPTerms$CC$enrichment <-
-    removePrevalentTerms(go$bestPTerms$CC$enrichment, max_preval)
-  go$bestPTerms$MF$enrichment <-
-    removePrevalentTerms(go$bestPTerms$MF$enrichment, max_preval)
-  go
-}
-
-enrichmentGO <- function(modules) {
-  require(org.Hs.eg.db)
-  require(WGCNA)
-
-  # Expect modules has columns: Gene.Symbol (ENSEMBL ids) and Cluster.ID
-  ensembl <- modules$Gene.Symbol
-  entrez_map <- AnnotationDbi::select(
-    org.Hs.eg.db,
-    keys = unique(ensembl),
-    columns = "ENTREZID",
-    keytype = "ENSEMBL"
-  )
-  entrez <- entrez_map$ENTREZID[match(ensembl, entrez_map$ENSEMBL)]
-
-  GOenrichmentAnalysis(
-    modules$Cluster.ID,
-    entrez,
-    organism = "human",
-    removeDuplicates = FALSE,
-    nBestP = 10,
-    backgroundType = "givenInGO"
-  )
-}
-
-enrichmentKegg <- function(modules) {
-  require(org.Hs.eg.db)
-
-  ensembl <- modules$Gene.Symbol
-  entrez_map <- AnnotationDbi::select(
-    org.Hs.eg.db,
-    keys = unique(ensembl),
-    columns = "ENTREZID",
-    keytype = "ENSEMBL"
-  )
-  between1 <- merge(
-    modules,
-    entrez_map,
-    by.x = "Gene.Symbol",
-    by.y = "ENSEMBL"
-  )
-
-  kegg_enrich <- compareCluster(
-    geneClusters = ENTREZID ~ Cluster.ID,
-    data = between1,
-    fun = "enrichKEGG",
-    organism = "hsa",
-    keyType = "kegg",
-    pvalueCutoff = 1,
-    qvalueCutoff = 1
-  )
-  setReadable(kegg_enrich, OrgDb = org.Hs.eg.db, keyType = "ENTREZID")
-}
 
 # ---------------------------------------------
 clusters_table_path <-
-  "/Users/edeneldar/CoExpression_ReProduction/xwgcna_old_original_run9_Clusters_table.txt"
+  "/Users/edeneldar/Library/CloudStorage/GoogleDrive-edenede2@gmail.com/Shared drives/is-SomekhLab/inter_tissue_coexpression/proccessed_data/ROSMAP_full_outputs/xwgcna_rosmap_autobeta_run5_Cluster_table.txt"
 clusters_details <- read.delim(
-  "/Users/edeneldar/CoExpression_ReProduction/xwgcna_old_original_run9_Clusters_details.txt",
+  "/Users/edeneldar/Library/CloudStorage/GoogleDrive-edenede2@gmail.com/Shared drives/is-SomekhLab/inter_tissue_coexpression/proccessed_data/ROSMAP_full_outputs/xwgcna_rosmap_autobeta_run5_Cluster_details.tsv",
   sep = "\t",
   header = TRUE
 )
@@ -268,7 +290,7 @@ go_tab <- data.frame(
 
 write.table(
   kegg_enrich@compareClusterResult,
-  "kegg_old.csv",
+  "kegg_rosmap_5000.csv",
   sep = ",",
   quote = FALSE,
   row.names = FALSE
@@ -276,7 +298,7 @@ write.table(
 
 write.table(
   go_enrich_filter$bestPTerms$BP$enrichment,
-  "go_bp_tab_old.csv",
+  "go_bp_tab_rosmap_5000.csv",
   sep = ",",
   quote = FALSE,
   row.names = FALSE
@@ -284,7 +306,7 @@ write.table(
 
 write.table(
   go_enrich_filter$bestPTerms$CC$enrichment,
-  "go_cc_tab_old.csv",
+  "go_cc_tab_rosmap_5000.csv",
   sep = ",",
   quote = FALSE,
   row.names = FALSE
@@ -292,7 +314,7 @@ write.table(
 
 write.table(
   go_enrich_filter$bestPTerms$MF$enrichment,
-  "go_mf_tab_old.csv",
+  "go_mf_tab_rosmap_5000.csv",
   sep = ",",
   quote = FALSE,
   row.names = FALSE
@@ -300,7 +322,7 @@ write.table(
 
 write.table(
   go_enrich_filter$enrichmentP,
-  "go_pmat_old.tsv",
+  "go_pmat_rosmap_5000.tsv",
   sep = "\t"
 )
 
